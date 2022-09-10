b4283016ef08b66930664c368084a2829a49d4f9
RinStar2003 <mituha133@gmail.com>
2022-09-10 20:14:24 +0300

    Initial Commit

---
+++ Flowey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flowey
+//
+//  Created by мас on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
